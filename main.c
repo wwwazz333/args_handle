7aa6fa8045eb2412fcf424058c60edfbba248062
wwwazz <virgile.menard@gmail.com>
2024-09-20 18:54:00 +0200

    transform to a lib

--- main.c
+++ main.c
@@ -1,97 +1,75 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
-enum arg_type
+char *concat_with_realloc(char *str1, char *str2, int add_space)
 {
-	LONG,
-	OTHER
-};
+	assert(add_space == 1 || add_space == 0);
 
+	char *temp = malloc((strlen(str1) + 1) * sizeof(char));
+	strcpy(temp, str1);
+	str1 = realloc(str1, (strlen(str1) + strlen(str2) + add_space + 1) * sizeof(char));
+	strcpy(str1, temp);
+	if (add_space)
+		strcat(str1, " ");
+	strcat(str1, str2);
+	free(temp);
+	return str1;
+}
+
+/**
+ * @param argc number of arguments
+ * @param argv arguments
+ * @param handle_action function that will handle the action (action, params) params is NULL if there is no params (ex: -e) and action is NULL if it's a default action
+ * @brief Handle the arguments passed to the program
+ */
 void handle_args(int argc, char **argv, void (*handle_action)(char *, char *))
 {
 	// skip the name of the program
 	argv++;
 	argc--;
 
-	enum arg_type mode = OTHER;
-	char *str_action = NULL;
-	char *str_handelling = NULL;
-
 	for (int i = 0; i < argc; i++)
 	{
 		if (strlen(argv[i]) >= 1 && argv[i][0] == '-')
 		{
-			if (str_handelling != NULL && strlen(str_handelling) > 0)
+			if (strlen(argv[i]) >= 2 && argv[i][1] == '-') // version long --example 123
 			{
-				handle_action(str_action, str_handelling);
-				free(str_handelling);
-				str_handelling = NULL;
-				free(str_action);
-				str_action = NULL;
-			}
+				char *action_long = malloc((strlen(argv[i]) - 1) * sizeof(char));
+				strcpy(action_long, argv[i] + 2 /*skip '--'*/);
 
-			mode = OTHER;
-			if (strlen(argv[i]) >= 2 && argv[i][0] == '-' && argv[i][1] == '-')
-			{
-				mode = LONG;
-				str_action = malloc((strlen(argv[i]) - 1) * sizeof(char));
-				strcpy(str_action, argv[i] + 2);
-				str_handelling = malloc(sizeof(char));
-				str_handelling[0] = '\0';
+				if (i + 1 >= argc || argv[i + 1][0] == '-') // no params
+				{
+					handle_action(action_long, NULL);
+				}
+				else
+				{
+					handle_action(action_long, argv[i + 1]);
+					i++;
+				}
 			}
-			else
+			else // versionc courte -e (each action is one letter)
 			{
-				str_handelling = malloc((strlen(argv[i]) + 1) * sizeof(char));
-				strcpy(str_handelling, argv[i] + 2);
-				str_action = malloc(2 * sizeof(char));
-				strncpy(str_action, argv[i] + 1, 1);
-				str_action[1] = '\0';
+				int nbr_actions = strlen(argv[i]) - 1;
+				char *actions = malloc((nbr_actions + 1) * sizeof(char));
+				strcpy(actions, argv[i] + 1 /* skip the '-' */);
+				for (int j = 0; j < nbr_actions; j++)
+				{
+					char action[2] = {actions[j], '\0'};
+					handle_action(action, NULL);
+				}
+
+				free(actions);
 			}
 		}
-		else // ne commence pas par '-'
+		else // default handle
 		{
-			if (mode == LONG)
-			{
-				char *temp = malloc((strlen(str_handelling) + strlen(argv[i]) + strlen(" ") + 1) * sizeof(char));
-				strcpy(temp, str_handelling);
-				strcat(temp, " ");
-				strcat(temp, argv[i]);
-				free(str_handelling);
-				str_handelling = temp;
-			}
-			else if (mode == OTHER)
-			{
-				// handle by default
-			}
+
+			char *temp = malloc(sizeof(char) * (strlen(argv[i]) + 1));
+			strcpy(temp, argv[i]);
+			handle_action(NULL, temp);
+			free(temp);
 		}
 	}
-	if (str_handelling != NULL && str_action != NULL)
-	{
-		handle_action(str_action, str_handelling);
-	}
-
-	// Libérer la mémoire allouée à la fin
-	if (str_handelling != NULL)
-	{
-		free(str_handelling);
-	}
-	if (str_action != NULL)
-	{
-		free(str_action);
-	}
-}
-
-void actions(char *action, char *params)
-{
-	printf("Yes: %s\t : %s\n", action, params);
-}
-
-int main(int argc, char **argv)
-{
-	// simmule un argv qui contiendrais `--action_test test test1 test2 -sShort`
-	char *params[] = {"./main", "--action_test", "test", "test1", "test2", "-sShort"};
-	// store function in a variable
-	handle_args(7, params, actions);
-	return 0;
 }
